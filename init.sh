3860172a1fd2db126d58108be27d6240e2cda30d
jiangyaqiii <45758831+jiangyaqiii@users.noreply.github.com>
2024-05-08 17:04:10 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -36,7 +36,7 @@
 # source /etc/profile
 
 
-#---------------------------------------------------------
+#-------------------安装mongo--------------------------------------
 #导入 MongoDB 的 GPG 公钥
 sudo rpm --import https://www.mongodb.org/static/pgp/server-4.4.asc
 #创建一个 repo 文件来获取 MongoDB 软件包
@@ -52,3 +52,54 @@ sudo yum install -y mongodb-org
 # 启动 MongoDB 服务
 sudo systemctl start mongod
 sudo systemctl status mongod
+
+#-------------------安装python3.6.8--------------------------------------
+sudo yum -y groupinstall "Development tools"
+sudo yum -y install zlib-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel readline-devel tk-devel gdbm-devel db4-devel libpcap-devel xz-devel libffi-devel
+wget https://www.python.org/ftp/python/3.6.8/Python-3.6.8.tar.xz
+# 解压包
+tar -xvf Python-3.6.8.tar.xz
+## 删除压缩包
+rm -f Python-3.6.8.tar.xz
+# 进入解压目录
+cd Python-3.6.8
+# 生成MakeFile
+sudo ./configure --prefix=/usr/local/python3.6.8 --enable-optimizations
+# 安装 使用make atinstall避免替换默认的python执行文件
+sudo make altinstall
+# 设置软连接到/user/bin/文件夹
+sudo ln -s /usr/local/python3.6.8/bin/python3.6 /usr/bin/python3
+sudo ln -s /usr/local/python3.6.8/bin/pip3.6 /usr/bin/pip3
+# 更新pip
+pip3 install --upgrade --user pip setuptools
+# 更改pip源
+mkdir ~/.pip
+cd ~/.pip
+echo '[global]
+index-url=http://mirrors.aliyun.com/pypi/simple/
+
+[install]
+trusted-host=mirrors.aliyun.com'
+> pip.conf
+
+#-------------------安装需要用到的库--------------------------------------
+# 下载Django
+pip3 install Django==3.2.4
+# 下载cronb
+pip3 install crontab
+pip3 install oss2==2.17.0
+pip3 install python-dateutil==2.8.2
+pip3 install bson
+pip3 install pymongo==3.6.0
+pip3 install redis==4.3.1
+pip3 install msgpack
+pip3 install zmail
+# 下载svn
+sudo yum install subversion
+mkdir xinrun
+cd xinrun
+# svn拉取代码
+echo svn用户名：hecao
+echo svn密码：tSsuuTNv9p
+svn checkout svn://116.62.198.65/repos/blackcat/branches/gedou_net_dev2/net
+
